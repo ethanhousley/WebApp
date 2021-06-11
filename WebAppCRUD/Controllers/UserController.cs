e5ed48fc4ce867267056c72d3c581ff8a67fff88
ethanhousley <66891702+ethanhousley@users.noreply.github.com>
2021-06-11 15:51:48 -0500

    Add project files.

---
+++ WebAppCRUD/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppCRUD.Models;
+
+namespace WebAppCRUD.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Random()
+        {
+            var user = new User() { UserID = 1346099 };
+            return View(user);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id" + id);
+        }
+    }
+}
